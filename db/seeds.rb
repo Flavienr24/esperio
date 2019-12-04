6382eb249d642cb6f1ae2cac01a79a4cdc1cd189
Laurent Deguil <ldeguil@yahoo.com>
2019-12-04 15:49:37 +0100

    skills ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#scrapping
+require 'open-uri'
+require 'nokogiri'
+
+name = "Martin_Jean"  # programmer la recuperation de la saisie sur le sign up
+url = "https://www.researchgate.net/profile/#{name}"
+
+html_file = open(url).read
+html_doc = Nokogiri::HTML(html_file)
+
+skills = []
+skill = 0
+
+html_doc.search('div.nova-l-flex:nth-child(2)').each do |element|
+  element.search('a:nth-child(1)').each do |elem|
+    unless elem.text.include?("Show more")
+      skill = elem.text.strip
+      #puts elem.attribute('href').value
+      puts elem.text.strip
+    end
+  skills << skill
+end
+end
+
+
+
+# html_doc.search('div.nova-e-avatar > img:nth-child(1)').each do |element|
+#       puts = elem.text.strip
+#       puts = elem.attribute('src').value
+#     end
+
