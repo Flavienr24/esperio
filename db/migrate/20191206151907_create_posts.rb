6d55e90d94b7dea6a64ce8763a9fe5fe12832623
annelise coudry <alcoudry@gmail.com>
2019-12-06 16:20:42 +0100

    initialize controller/model/view - posts

---
+++ db/migrate/20191206151907_create_posts.rb
@@ -0,0 +1,10 @@
+class CreatePosts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :posts do |t|
+      t.text :content
+      t.string :file
+
+      t.timestamps
+    end
+  end
+end
