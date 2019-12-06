34234c0514502a764f65bee124bc72096c47d883
Laurent Deguil <ldeguil@yahoo.com>
2019-12-06 13:54:51 +0100

    craping wip

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,39 +2,6 @@ require 'open-uri'
 require 'nokogiri'
 class RegistrationsController < Devise::RegistrationsController
 
-  def get_user_information
-    url = "https://www.researchgate.net/profile/#{params[:username]}"
-
-    html_file = open(url).read
-    html_doc = Nokogiri::HTML(html_file)
-
-    session[:skills] = []
-
-    html_doc.search('div.nova-l-flex:nth-child(2)').each do |element|
-      element.search('a:nth-child(1)').each do |elem|
-        unless elem.text.include?("Show more")
-          skill = elem.text.strip
-          session[:skills] << skill
-        end
-      end
-    end
-
-    #scrapping avatar
-    html_doc.search('div.nova-e-avatar > img:nth-child(1)').each do |element|
-      avatar = element.values.first
-      session[:avatar] = avatar
-    end
-
-    #scrapping name
-    html_doc.search('div.nova-e-text--spacing-xxs').each do |element|
-    name = element.text.strip
-    session[:first_name] = name.split.first
-    session[:last_name] = name.split.last
-  end
-
-  redirect_to new_user_registration_path
-end
-
 def new
   super
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,112 +10,112 @@
 require 'open-uri'
 require 'nokogiri'
 require 'faker'
-require 'pry-byebug'
+# require 'pry-byebug'
 
 
-name = "Martin_Jean"  # programmer la recuperation de la saisie sur le sign up en JS
-url = "https://www.researchgate.net/profile/#{name}"
-# url = File.read("/home/deguil/Bureau/toto.html") code pour relier une page en local
+# name = "Martin_Jean"  # programmer la recuperation de la saisie sur le sign up en JS
+# url = "https://www.researchgate.net/profile/#{name}"
+# # url = File.read("/home/deguil/Bureau/toto.html") code pour relier une page en local
 
-html_file = open(url).read
-html_doc = Nokogiri::HTML(html_file)
-skills = []
+# html_file = open(url).read
+# html_doc = Nokogiri::HTML(html_file)
+# skills = []
 
-html_doc.search('div.nova-l-flex:nth-child(2)').each do |element|
-  puts "salut"
-  element.search('a:nth-child(1)').each do |elem|
-    unless elem.text.include?("Show more")
-      skill = elem.text.strip
-     puts elem.text.strip
-    skills << skill
-    end
-  end
-end
-
-#scrapping avatar
-html_doc.search('div.nova-e-avatar > img:nth-child(1)').each do |element|
-
-  avatar = element.values.first
-  puts avatar
-  end
-
-# scrapping name
-html_doc.search('div.nova-e-text--spacing-xxs').each do |element|
-  name = element.text.strip
-  puts name
-  end
-
-
-
-
-# puts 'Destroy Database...'
-
-# Company.destroy_all
-# User.destroy_all
-# Project.destroy_all
-# Collaboration.destroy_all
-
-# puts 'Creating Database...'
-
-# company_test = Company.create(name:"The big company")
-
-# user_test = User.create(first_name:"John", last_name:"Lascience", country:"France", city:"Paris", function:"Fellow researcher", school:"Harvar University", phone:"+33 1 42 61 65 34", email:"john@john.com", password:"password", company_id: company_test.id)
-
-# project_test = Project.create(name:"Hydrolic model conception", description:"We built Devise to help you quickly develop an application that uses authentication. However, we don't want to be in your way when you need to customize it.", user_id: user_test.id, status: 'In Progress', visibility: true, open_to_apply: false)
-
-# collaboration_test = Collaboration.create(project_id: project_test.id, user_id: user_test.id, joined: 'Pending')
-
-# puts Company.count
-# puts User.count
-# puts Project.count
-# puts Collaboration.count
-
-
-# puts 'Clean database...'
-
-# Company.destroy_all
-# User.destroy_all
-# Project.destroy_all
-# Collaboration.destroy_all
-
-# feelows = Company.create(name: 'foo bar')
-
-
-# puts Company.count
-
-# puts 'Creating 10 fake user...'
-
-# 10.times do
-#   user = User.new(
-#     fullname: Faker::Name.name,
-#     email: Faker::Internet.email,
-#     password: Faker::Internet.password,
-#     avatar: Faker::Avatar.image(slug: "my-own-slug", size: "50x50"),
-#     phone: Faker::PhoneNumber.phone_number_with_country_code,
-#     country: Faker::Address.country,
-#     city: Faker::Address.city,
-#     function: Faker::Job.title,
-#     school: Faker::University.name,
-#     company: feelows
-#   )
-#   user.save!
-
-
-#   2.times do
-#     project = Project.new(
-#     name: Faker::Restaurant.name,
-#     description: Faker::Lorem.sentence,
-#     status: 'In Progress',
-#     visibility: true,
-#     open_to_apply: false,
-#     user: user
-#     )
-#     project.save!
+# html_doc.search('div.nova-l-flex:nth-child(2)').each do |element|
+#   puts "salut"
+#   element.search('a:nth-child(1)').each do |elem|
+#     unless elem.text.include?("Show more")
+#       skill = elem.text.strip
+#      puts elem.text.strip
+#     skills << skill
+#     end
 #   end
 # end
 
+# #scrapping avatar
+# html_doc.search('div.nova-e-avatar > img:nth-child(1)').each do |element|
 
-# puts 'Finished!'
+#   avatar = element.values.first
+#   puts avatar
+#   end
 
-#  puts "company created : #{Company.count} "
-#  puts "user created : #{User.count} "
+# # scrapping name
+# html_doc.search('div.nova-e-text--spacing-xxs').each do |element|
+#   name = element.text.strip
+#   puts name
+#   end
+
+
+
+
+puts 'Destroy Database...'
+
+Company.destroy_all
+User.destroy_all
+Project.destroy_all
+Collaboration.destroy_all
+
+puts 'Creating Database...'
+
+company_test = Company.create(name:"The big company")
+
+user_test = User.create(fullname:"John_lascience", country:"France", city:"Paris", function:"Fellow researcher", school:"Harvar University", phone:"+33 1 42 61 65 34", email:"john@john.com", password:"password", company_id: company_test.id)
+
+project_test = Project.create(name:"Hydrolic model conception", description:"We built Devise to help you quickly develop an application that uses authentication. However, we don't want to be in your way when you need to customize it.", user_id: user_test.id, status: 'In Progress', visibility: true, open_to_apply: false)
+
+collaboration_test = Collaboration.create(project_id: project_test.id, user_id: user_test.id, joined: 'Pending')
+
+puts Company.count
+puts User.count
+puts Project.count
+puts Collaboration.count
+
+
+puts 'Clean database...'
+
+Company.destroy_all
+User.destroy_all
+Project.destroy_all
+Collaboration.destroy_all
+
+feelows = Company.create(name: 'foo bar')
+
+
+puts Company.count
+
+puts 'Creating 10 fake user...'
+
+10.times do
+  user = User.new(
+    fullname: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: Faker::Internet.password,
+    avatar: Faker::Avatar.image(slug: "my-own-slug", size: "50x50"),
+    phone: Faker::PhoneNumber.phone_number_with_country_code,
+    country: Faker::Address.country,
+    city: Faker::Address.city,
+    function: Faker::Job.title,
+    school: Faker::University.name,
+    company: feelows
+  )
+  user.save!
+
+
+  2.times do
+    project = Project.new(
+    name: Faker::Restaurant.name,
+    description: Faker::Lorem.sentence,
+    status: 'In Progress',
+    visibility: true,
+    open_to_apply: false,
+    user: user
+    )
+    project.save!
+  end
+end
+
+
+puts 'Finished!'
+
+ puts "company created : #{Company.count} "
+ puts "user created : #{User.count} "
