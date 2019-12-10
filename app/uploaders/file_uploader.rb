e28601bdf49bec01ee96d77742c3133e2fb3495b
annelise coudry <alcoudry@gmail.com>
2019-12-10 14:59:56 +0100

    create ajax + cloudinary pour contributions projet

--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -1,12 +1,5 @@
 class FileUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
-  process eager: true
-
-  process convert: 'jpg'
-
-  version :thumnail do
-  resize_to_fit 256, 256
-  end
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
