96d2ce8f7e1055f2bbec351cf66541d5eb53b556
Stéphane <stephane.26@hotmail.fr>
2019-12-11 17:04:49 +0100

    update design & bugfix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
   def index
-     @users = User.all
-     @users = @users.fullname_search(params[:query]) if params[:query].present?
-     @users = User.all.tagged_with(params[:query], any: true) if @users.empty?
-     @user = User.find(current_user.id)
+    @users = User.all
+    @users = @users.fullname_search(params[:query]) if params[:query].present?
+    @users = User.all.tagged_with(params[:query], any: true) if @users.empty?
+    @user = User.find(current_user.id)
   end
 
   def show
@@ -15,10 +15,9 @@ class UsersController < ApplicationController
 
   def my_profile
     @user = current_user
+    @user.avatar = src="flavien.png"
     @projects_I_lead = @user.projects.order("created_at DESC")
     @collaborations = @user.collaborations
     @myexpertises = @user.skill_list
-    @user.avatar = src="flavien.png"
-
   end
 end
